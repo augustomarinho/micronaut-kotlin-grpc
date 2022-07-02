9f57f0def245dadd670d8b4fd11a3b9e63f0bc3c
Augusto Marinho <augustomarinho@gmail.com>
2022-07-02 13:35:45 -0300

    Implementing the error flow

--- src/main/kotlin/com/am/app/grpc/DemoGRPCServer.kt
+++ src/main/kotlin/com/am/app/grpc/DemoGRPCServer.kt
@@ -3,15 +3,42 @@ package com.am.app.grpc
 import com.am.app.CreateUserRequest
 import com.am.app.CreateUserResponse
 import com.am.app.UserServiceGrpcKt
+import com.google.rpc.BadRequest
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
 import jakarta.inject.Singleton
 
 @Singleton
 class DemoGRPCServer : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
 
     override suspend fun createUser(request: CreateUserRequest): CreateUserResponse {
-        return CreateUserResponse
+        return when(request.name.lowercase()) {
+            "bug" -> throw StatusRuntimeException(Status.INTERNAL);
+            "badrequest" -> throw StatusProto.toStatusRuntimeException(BuildErrors.buildBadRequest())
+            else ->  CreateUserResponse
+                .newBuilder()
+                .setMessage("User Created: ${request.name}")
+                .build()
+        }
+    }
+}
+
+object BuildErrors {
+    fun buildBadRequest(): com.google.rpc.Status {
+        return com.google.rpc.Status
             .newBuilder()
-            .setMessage("User Created: ${request.name}")
+            .setCode(Code.INVALID_ARGUMENT_VALUE)
+            .setMessage("Requisicao Invalida!")
+            .addDetails(com.google.protobuf.Any.pack(BadRequest
+                .newBuilder()
+                .addFieldViolations(BadRequest.FieldViolation
+                    .newBuilder()
+                    .setField("nome")
+                    .setDescription("Nome Invalido")
+                    .build())
+                .build()))
             .build()
     }
 }
